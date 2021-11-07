50d9c695a7b75ceb1a6ecbcf0596aed1043c60aa
agustinrobledo <agustinrobledo59.ar@gmail.com>
2021-11-07 15:53:55 -0300

    added dynamic page with id

---
+++ components/Home/index.tsx
@@ -0,0 +1,75 @@
+import { useEffect, useState } from "react";
+import Link from "next/link";
+type inputEvent = React.ChangeEvent<HTMLInputElement>;
+type FormSubmitEvent = React.FormEvent<HTMLFormElement>;
+interface CityData {
+    name: string;
+    main: {
+        temp: number;
+        temp_min: number;
+        temp_max: number;
+    };
+    id: number;
+    weather: [
+        {
+            description: string;
+            icon: string;
+        }
+    ];
+}
+const apiKey: String = "7796135672a1ef998330890d3fb9eb22"
+export default function HomePage(): JSX.Element {
+    const [city, setCity] = useState<string> ("");
+    const onChange = (e: inputEvent) => {
+        setCity(e.target.value);
+    }
+    const [cities, setCities] = useState<CityData[]>([]);
+    const onSubmit = (e: FormSubmitEvent) => {
+        e.preventDefault();
+        if (city) {
+            fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
+                .then(res => res.json())
+                .then(data => {
+                    setCities([...cities, data]);
+                    setCity("");
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+        }
+    }
+  return (
+    <div>
+        <h1>
+          Home
+        </h1>
+        <form
+        onSubmit={onSubmit}
+        >
+            <input 
+            placeholder="Busca tu ciudad"
+            type="search"
+            value={city}
+            onChange={onChange}
+            />
+        </form>
+        <div>
+            {cities.map((city, index) => (
+                <div 
+                key={city.id}
+                >
+                    <h2>
+                        <Link href={`/${city.id}`}>
+                            {city.name}
+                        </Link>
+                    </h2>
+                    <p>Temperatura actual: {city.main.temp}</p>
+                    <p>Min: {city.main.temp_min}</p>
+                    <p>Max: {city.main.temp_max}</p>
+                    <img src={`http://openweathermap.org/img/wn/${city.weather[0].icon}@2x.png`}/>
+                </div>
+            ))}
+      </div>
+    </div>
+  );
+}
---
+++ pages/[city].tsx
@@ -0,0 +1,48 @@
+import { GetServerSideProps } from "next";
+interface CityData {
+    weather:{
+    name: string;
+    main: {
+      temp: number;
+      temp_min: number;
+      temp_max: number;
+    };
+    weather: [
+      {
+        description: string;
+        icon: string;
+      }
+    ];
+  }
+}
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const city = context.query.city;
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?id=${city}&appid=${apiKey}&units=metric`
+  );
+  const data = await response.json();
+  return {
+    props: {
+      weather: data,
+    },
+  };
+};
+
+const apiKey: String = "7796135672a1ef998330890d3fb9eb22"
+export default function City({weather}: CityData): JSX.Element {
+  console.log(weather)
+  return (
+    <div>
+      <h1>City</h1>
+      <p>{weather.name}</p>
+      <p>{weather.weather[0].description}</p>
+      <p>{weather.main.temp}</p>
+      <p>{weather.main.temp_min}</p>
+      <p>{weather.main.temp_max}</p>
+      <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}/>
+    </div>
+  );
+}
+
+
+
